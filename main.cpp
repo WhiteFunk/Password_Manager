e262669dc0a46c0e6ad3a3711ad6100d7d7733ba
Ilya <odinets.ilya.careers@gmail.com>
2022-09-27 10:36:09 +0300

    Update main.cpp
    Added contact and license information

--- main.cpp
+++ main.cpp
@@ -1,3 +1,24 @@
+/******************************************************************************************/
+/*                                                                                        */
+/*    Password local storage/manager                                                      */
+/*                                                                                        */
+/*    ----------------------------------------------------------                          */
+/*                                                                                        */
+/*                                                                                        */
+/*    This program is free software: you can redistribute it and/or modify                */
+/*    it under the terms of the GNU General Public License as published by                */
+/*    the Free Software Foundation, either version 3 of the License, or                   */
+/*    (at your option) any later version.                                                 */
+/*                                                                                        */
+/*                                                                                        */
+/*                                                                                        */
+/*    Contact:   odinets.ilya.careers@gmail.com                                           */
+/*               www.linkedin.com/in/ilya-odinets-238ba124a                               */
+/*                                                                                        */
+/*                                                                                        */
+/******************************************************************************************/
+
+
 #include "mainwindow.h"
 
 #include <QApplication>
